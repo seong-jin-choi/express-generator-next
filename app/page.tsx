e871308322109eca9d5e3f87c98fdccee4546557
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2024-02-23 15:21:44 +0900

    폰트 확인 페이지

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,104 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { montserrat, notoSansKR, pretendard } from "./fonts";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={pretendard.className}>
+      <input id="test" type="text" />
+      <h2 id="test__h2">test1234</h2>
+      <details open={true}>
+        <summary>"details 요소란?"</summary>
+        <p>열림 상태일 때 정보를 표시하는 위젯이다.</p>
+      </details>
+      <label htmlFor="local">지역번호:</label>
+      <input type="text" id="local" list="local-list" />
+      <datalist id="local-list">
+        <option value="02" label="서울"></option>
+        <option value="031" label="경기"></option>
+      </datalist>
+      <div className={pretendard.className} style={{ fontSize: "1.2rem" }}>
+        <hr></hr>
+        <h1 style={{ fontSize: "30px", marginTop: "10px", marginBlock: "10px" }}>pretendard</h1>
+        <h1 style={{ fontWeight: 100 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 200 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 300 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 400 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 500 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 600 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 700 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 800 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 900 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 100 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 200 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 300 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 400 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 500 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 600 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 700 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 800 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 900 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 100 }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: 300 }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: "normal" }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: 600 }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: "bold" }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: 900 }}>홈페이지 한글 폰트 두께 테스트</h1>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={montserrat.className} style={{ fontSize: "1.2rem" }}>
+        <hr></hr>
+        <h1 style={{ fontSize: "30px", marginTop: "10px", marginBlock: "10px" }}>montserrat</h1>
+        <h1 style={{ fontWeight: 100 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 200 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 300 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 400 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 500 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 600 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 700 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 800 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 900 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 100 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 200 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 300 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 400 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 500 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 600 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 700 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 800 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 900 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 100 }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: 300 }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: "normal" }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: 600 }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: "bold" }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: 900 }}>홈페이지 한글 폰트 두께 테스트</h1>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={notoSansKR.className} style={{ fontSize: "1.2rem" }}>
+        <hr></hr>
+        <h1 style={{ fontSize: "30px", marginTop: "10px", marginBlock: "10px" }}>notoSansKR</h1>
+        <h1 style={{ fontWeight: 100 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 200 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 300 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 400 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 500 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 600 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 700 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 800 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 900 }}>home page english font weight test</h1>
+        <h1 style={{ fontWeight: 100 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 200 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 300 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 400 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 500 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 600 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 700 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 800 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 900 }}>1234567890 number test</h1>
+        <h1 style={{ fontWeight: 100 }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: 300 }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: "normal" }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: 600 }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: "bold" }}>홈페이지 한글 폰트 두께 테스트</h1>
+        <h1 style={{ fontWeight: 900 }}>홈페이지 한글 폰트 두께 테스트</h1>
       </div>
-    </main>
+    </div>
   );
 }
